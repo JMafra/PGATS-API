49c39f473cd2bae472d4ceb594fabfd9b59a3a75
Juliana <jmafra.pe@gmail.com>
2025-08-09 17:51:35 -0300

    Adicionando um novo teste de erro na transferencia com mock

--- test/controller/transferController.test.js
+++ test/controller/transferController.test.js
@@ -1,7 +1,13 @@
+//Bibliotecas
 const request = require('supertest');
 const sinon = require('sinon');
-const app = require('../../app');
 const { expect } = require('chai');
+
+//Aplicação
+const app = require('../../app');
+
+// Mock
+const transferService = require('../../service/transferService')
 
 describe('Transfer Controller', () => {
     describe('POST /transfers', () => {
@@ -16,6 +22,27 @@ describe('Transfer Controller', () => {
                 });
           expect(resposta.status).to.equal(400);
           expect(resposta.body).to.have.property('error', 'Usuário remetente ou destinatário não encontrado.')
+        });
+
+        it('Usando Mocks: Quando informo remetente e destinatario inexistentes recebo 400 ', async  () => {
+          // Mocar apenas a função transfer do Service
+          const transferServiceMock = sinon.stub(transferService, 'transferValue') 
+          transferServiceMock.throws(new Error('Usuário remetente ou destinatário não encontrado.'));
+
+          const resposta = await request(app)
+                .post('/transfers')
+                .send({
+                    from: "mafra",
+                    to: "artrur",
+                    value: 100
+
+                });
+          expect(resposta.status).to.equal(400);
+          expect(resposta.body).to.have.property('error', 'Usuário remetente ou destinatário não encontrado.')
+       
+          // Reset do Mock
+          sinon.restore();
+
         });
     });
 
